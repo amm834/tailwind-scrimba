81dd247f951e089f51a9f9aced3cdaaedd054113
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2022-04-04 23:37:28 +0630

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:amm834/tailwind-scrimba.git main:gh-pages
+# git push -f git@github.com:amm834/tailwind-scrimba.git main:gh-pages
+ 
+  git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
